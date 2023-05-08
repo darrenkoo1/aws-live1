e80c4474b6daf8615c3f55cb295b1def671c4477
darrenkoo1 <126452264+darrenkoo1@users.noreply.github.com>
2023-05-08 16:32:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.crm7qupyokkn.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "-employee"
+customregion = "us-east-1"
